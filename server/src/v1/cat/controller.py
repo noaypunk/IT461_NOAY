8dad608f37acb058cc70e5c6ffac51c27528d8d0
Ryan Noay <rnoaylovema@gmail.com>
2022-05-12 01:52:22 +0800

    exer 3

--- server/src/v1/cat/controller.py
+++ server/src/v1/cat/controller.py
@@ -1,11 +1,12 @@
 from flask import request, make_response, jsonify
-from v1.dog.model import DogModel
+from v1.basecontroller import BaseController
+from v1.cat.model import CatModel
 
-class DogController():
+class CatController(BaseController):
     _instance = None
 
     def __init__(self) -> None:
-        self._instance = DogModel()
+        self._instance = CatModel()
 
     def post(self):
         print(request.json)
@@ -16,30 +17,45 @@ class DogController():
             }), 400)
         return jsonify(resp)
 
-    def check(self, dog_id):
-        dog = self._instance.read({"id": dog_id})
-        if dog is None:
-            return make_response(jsonify({"error": "Dog id not found."}), 404)
-        return dog
+    def check(self, cat_id, filters=None):
+        if filters is not None:
+            filters['id'] = cat_id
+        else:
+            filters = {"id": cat_id}
+        cat = self._instance.read(filters)
+        if cat is None:
+            return make_response(jsonify({"error": "Cat id not found."}), 404)
+        return cat
 
-    def get(self, dog_id=None):
-        if dog_id is not None:
-            dog = self.check(dog_id)
-            if not isinstance(dog, dict):
+    def get(self, cat_id=None):
+        filters = {}
+        if 'fields' in request.args:
+            filters['fields'] = request.args['fields'].split(',')
+        if cat_id is not None:
+            cat = self.check(cat_id, filters)
+            if not isinstance(cat, dict):
+                return cat
+            return jsonify(cat)
+        filters['offset'] = int(request.args['offset']) if 'offset' in request.args else 0
+        filters['limit'] = int(request.args['limit']) if 'limit' in request.args else 5
+        cats = self._instance.read(filters)
+        total = self._instance.read(filters, True)
+        return jsonify({
+            'metadata': {
+                'total': total,
+                'links': self.build_links(total, filters['offset'], filters['limit'])
+            },
+            'data': cats
+        })
 
-                return dog
-            return jsonify(dog)
-        dogs = self._instance.read()
-        return jsonify(dogs)
-
-    def put(self, dog_id=None):
-        if dog_id is not None:
-            dog = self.check(dog_id)
-            if not isinstance(dog, dict):
-                return dog
-            dog_data = request.json
-            dog_data['id'] = dog_id
-            resp = self._instance.update(dog_data)
+    def put(self, cat_id=None):
+        if cat_id is not None:
+            cat = self.check(cat_id)
+            if not isinstance(cat, dict):
+                return cat
+            cat_data = request.json
+            cat_data['id'] = cat_id
+            resp = self._instance.update(cat_data)
             if resp == False:
                 return make_response(jsonify({
                     "error": "Failed to update. There are items in your request that are invalid."
@@ -47,10 +63,10 @@ class DogController():
             return jsonify(resp)
         return jsonify(self._instance.update(request.json))
 
-    def delete(self, dog_id=None):
-        if dog_id is not None:
-            dog = self.check(dog_id)
-            if not isinstance(dog, dict):
-                return dog
-            return jsonify(self._instance.delete(dog_id))
+    def delete(self, cat_id=None):
+        if cat_id is not None:
+            cat = self.check(cat_id)
+            if not isinstance(cat, dict):
+                return cat
+            return jsonify(self._instance.delete(cat_id))
         return jsonify(self._instance.delete(request.json))

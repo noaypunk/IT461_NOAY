df1a38df14dc38072cb2e96f377f6b014d530009
Ryan Noay <rnoaylovema@gmail.com>
2022-05-24 09:40:06 +0800

    exercise 6

--- server/src/v1/cat/controller.py
+++ server/src/v1/cat/controller.py
@@ -2,6 +2,7 @@ from flask import request, make_response, jsonify
 from v1.basecontroller import BaseController
 from v1.cat.model import CatModel
 
+
 class CatController(BaseController):
     _instance = None
 
@@ -12,14 +13,19 @@ class CatController(BaseController):
         print(request.json)
         resp = self._instance.create(request.json)
         if resp == False:
-            return make_response(jsonify({
-                "error": "Failed to add. There are items in your request that are invalid."
-            }), 400)
+            return make_response(
+                jsonify(
+                    {
+                        "error": "Failed to add. There are items in your request that are invalid."
+                    }
+                ),
+                400,
+            )
         return jsonify(resp)
 
     def check(self, cat_id, filters=None):
         if filters is not None:
-            filters['id'] = cat_id
+            filters["id"] = cat_id
         else:
             filters = {"id": cat_id}
         cat = self._instance.read(filters)
@@ -29,24 +35,30 @@ class CatController(BaseController):
 
     def get(self, cat_id=None):
         filters = {}
-        if 'fields' in request.args:
-            filters['fields'] = request.args['fields'].split(',')
+        if "fields" in request.args:
+            filters["fields"] = request.args["fields"].split(",")
         if cat_id is not None:
             cat = self.check(cat_id, filters)
             if not isinstance(cat, dict):
                 return cat
             return jsonify(cat)
-        filters['offset'] = int(request.args['offset']) if 'offset' in request.args else 0
-        filters['limit'] = int(request.args['limit']) if 'limit' in request.args else 5
+        filters["offset"] = (
+            int(request.args["offset"]) if "offset" in request.args else 0
+        )
+        filters["limit"] = int(request.args["limit"]) if "limit" in request.args else 5
         cats = self._instance.read(filters)
         total = self._instance.read(filters, True)
-        return jsonify({
-            'metadata': {
-                'total': total,
-                'links': self.build_links(total, filters['offset'], filters['limit'])
-            },
-            'data': cats
-        })
+        return jsonify(
+            {
+                "metadata": {
+                    "total": total,
+                    "links": self.build_links(
+                        total, filters["offset"], filters["limit"]
+                    ),
+                },
+                "data": cats,
+            }
+        )
 
     def put(self, cat_id=None):
         if cat_id is not None:
@@ -54,12 +66,17 @@ class CatController(BaseController):
             if not isinstance(cat, dict):
                 return cat
             cat_data = request.json
-            cat_data['id'] = cat_id
+            cat_data["id"] = cat_id
             resp = self._instance.update(cat_data)
             if resp == False:
-                return make_response(jsonify({
-                    "error": "Failed to update. There are items in your request that are invalid."
-                }), 400)
+                return make_response(
+                    jsonify(
+                        {
+                            "error": "Failed to update. There are items in your request that are invalid."
+                        }
+                    ),
+                    400,
+                )
             return jsonify(resp)
         return jsonify(self._instance.update(request.json))
 
--- server/src/v1/cat/model.py
+++ server/src/v1/cat/model.py
@@ -1,6 +1,6 @@
 from db import Db
 
-class CatModel():
+class CatModel:
     def sanitize(self, cats):
         if not isinstance(cats, (list, tuple)):
             cats = [cats]
@@ -8,7 +8,7 @@ class CatModel():
         for cat in cats:
             if not isinstance(cat, dict):
                 continue
-            if not ('id' in cat and 'name' in cat):
+            if not ("id" in cat and "name" in cat):
                 continue
             clean_cats.append(cat)
         return clean_cats
@@ -22,39 +22,39 @@ class CatModel():
         queries = []
         for cat in clean_cats:
             sql = "INSERT INTO cats(name) VALUES(%s)"
-            queries.append({"sql": sql, "bind": cat['name']})
+            queries.append({"sql": sql, "bind": cat["name"]})
         db = Db.get_instance()
         result = db.transactional(queries)
         return cats
 
     def read(self, filters=None, count_only=False):
         db = Db.get_instance()
-        fields = ['*']
+        fields = ["*"]
         offset = 0
         limit = 5
         if filters is not None:
-            if 'fields' in filters:
+            if "fields" in filters:
                 tmp_fields = []
-                for field in filters['fields']:
-                    if field in ['id', 'name']:
+                for field in filters["fields"]:
+                    if field in ["id", "name"]:
                         tmp_fields.append(field)
                 if len(tmp_fields) > 0:
                     fields = tmp_fields
-            if 'id' in filters:
-                sql = "SELECT " + ','.join(fields) + " FROM cats WHERE id = %s"
-                cat = db.fetchone(sql, filters['id'])
+            if "id" in filters:
+                sql = "SELECT " + ",".join(fields) + " FROM cats WHERE id = %s"
+                cat = db.fetchone(sql, filters["id"])
                 return cat
-            if 'offset' in filters:
-                offset = int(filters['offset'])
-            if 'limit' in filters:
-                limit = int(filters['limit'])
-        cols = 'COUNT(*) AS total' if count_only else ','.join(fields)
+            if "offset" in filters:
+                offset = int(filters["offset"])
+            if "limit" in filters:
+                limit = int(filters["limit"])
+        cols = "COUNT(*) AS total" if count_only else ",".join(fields)
         sql = "SELECT " + cols + " FROM cats"
         if not count_only:
             sql += " ORDER BY name LIMIT " + str(offset) + ", " + str(limit)
         if count_only:
             row = db.fetchone(sql)
-            return row['total'] if row else 0
+            return row["total"] if row else 0
         else:
             return db.fetchall(sql)
 
@@ -67,7 +67,7 @@ class CatModel():
         queries = []
         for cat in clean_cats:
             sql = "UPDATE cats SET name = %s WHERE id = %s"
-            queries.append({"sql": sql, "bind": (cat['name'], cat['id'])})
+            queries.append({"sql": sql, "bind": (cat["name"], cat["id"])})
         db = Db.get_instance()
         db.transactional(queries)
         return cats
@@ -79,7 +79,7 @@ class CatModel():
         placeholder = []
         queries = []
         for cat in cats:
-            placeholder.append('%s')
+            placeholder.append("%s")
         sql = "DELETE FROM cats WHERE id IN (" + ", ".join(placeholder) + ")"
         queries.append({"sql": sql, "bind": cats})
         db = Db.get_instance()
